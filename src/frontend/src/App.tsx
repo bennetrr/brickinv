036fe57c7479c88048706a846eb24cd45af4fe8f
bennetrr <bennet@bennetr.me>
2024-01-02 21:54:23 +0100

    TODOs

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -15,6 +15,30 @@ const appStore = AppStore.create();
 AuthenticationService.addTokenChangeHandler(token => appStore.authenticationStore.setIsAuthenticated(!!token));
 appStore.authenticationStore.setIsAuthenticated(AuthenticationService.isAuthenticated);
 
+// TODO: Backend
+//       - Loosen up password requirements
+//       - Error handling and logging
+//       - Groups and Users
+//       - Space Blocks Permissions
+//       - CQRS (?)
+//       - Make use of ErrorHandlerMiddleware (?)
+
+// TODO: Frontend
+//       - Loading, success and error indicators
+//       - Error handling and logging
+//       - Test for and fix small errors
+//       - Customize theme
+//       - Move components into own files
+//       - Error pages
+
+// TODO: General
+//       - Think of app name and logo
+//       - Docker containers and compose file
+//       - GitHub Workflows
+//       - Ask Sebastian things that I don't understand
+//       - Ask Sebastian for feedback on code
+//       - Write documentation and tests (maybe)
+
 const App: React.FC = () => {
   useEffect(() => {
     if (!AuthenticationService.isAuthenticated) {
