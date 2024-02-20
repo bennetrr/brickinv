0daee255d1c08ba15ae3df008d774bd7e9f056e0
bennetrr <bennet@bennetr.me>
2024-02-20 10:51:49 +0100

    Add migration

---
+++ src/backend/Bennetr.BrickInv.Api/Bennetr.BrickInv.Api/Migrations/BrickInv/20240220094642_AddRebrickableApiKeyOverride.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bennetr.BrickInv.Api.Migrations.BrickInv
+{
+    /// <inheritdoc />
+    public partial class AddRebrickableApiKeyOverride : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RebrickableApiKey",
+                table: "Groups",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RebrickableApiKey",
+                table: "Groups");
+        }
+    }
+}
