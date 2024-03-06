006d0a40dff7e9ec9e23c4b44d5147b5472805cd
bennetrr <bennet@bennetr.me>
2024-03-06 15:03:51 +0100

    fix: styling

--- src/backend/Bennetr.BrickInv.Api/Bennetr.BrickInv.Api/Services/Email/ProfileEmailSender.cs
+++ src/backend/Bennetr.BrickInv.Api/Bennetr.BrickInv.Api/Services/Email/ProfileEmailSender.cs
@@ -9,36 +9,36 @@ public class ProfileEmailSender(IHtmlEmailGenerator emailGenerator, IGenericEmai
     {
         await emailSender.SendEmailAsync(email, $"{invite.Issuer.Username} invited you to a BrickInv group",
             emailGenerator.Generate(
-                $"""
-                 <p>
-                   Hi {invite.Recipient.Username},<br>
-                   {invite.Issuer.Username} invited you to their BrickInv group {invite.Group.Name}.
-                 </p>
+               $"""
+               <p>
+                 Hi {invite.Recipient.Username},<br>
+                 {invite.Issuer.Username} invited you to their BrickInv group {invite.Group.Name}.
+               </p>
 
-                 <p>
-                   Click the button below to join their group.
-                 </p>
+               <p>
+                 Click the button below to join their group.
+               </p>
 
-                 <div class="center">
-                   <a href="{acceptLink}" role="button" class="button">
-                     Join {invite.Group.Name}
-                   </a>
-                 </div>
-                 """,
-                $"""
-                 <p>
-                   If you can't click the button, copy the following link into your browser:<br>
-                   <span class="text-small">{acceptLink}</span>
-                 </p>
+               <div class="center">
+                 <a href="{acceptLink}" role="button" class="button">
+                   Join {invite.Group.Name}
+                 </a>
+               </div>
+               """,
+               $"""
+               <p>
+                 If you can't click the button, copy the following link into your browser:<br>
+                 <span class="text-small">{acceptLink}</span>
+               </p>
 
-                 <p>
-                   The link expires in 48 hours.
-                 </p>
+               <p>
+                 The link expires in 48 hours.
+               </p>
 
-                 <p>
-                   If you don't want to join the group, you can ignore this email.
-                 </p>
-                 """
+               <p>
+                 If you don't want to join the group, you can ignore this email.
+               </p>
+               """
             )
         );
     }
