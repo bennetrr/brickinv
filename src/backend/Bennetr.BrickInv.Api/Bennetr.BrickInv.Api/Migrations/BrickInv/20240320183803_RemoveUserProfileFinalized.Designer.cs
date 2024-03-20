d08948b6b7de2db9ac75ec6d669111e9f5f67fc7
bennetrr <bennet@bennetr.me>
2024-03-20 19:38:55 +0100

    fix(backend): Add migration for 7d8e69

--- src/backend/Bennetr.BrickInv.Api/Bennetr.BrickInv.Api/Migrations/BrickInv/20240320183803_RemoveUserProfileFinalized.Designer.cs
+++ src/backend/Bennetr.BrickInv.Api/Bennetr.BrickInv.Api/Migrations/BrickInv/20240320183803_RemoveUserProfileFinalized.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bennetr.BrickInv.Api.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bennetr.BrickInv.Api.Migrations.BrickInv
 {
     [DbContext(typeof(BrickInvContext))]
-    partial class BrickInvContextModelSnapshot : ModelSnapshot
+    [Migration("20240320183803_RemoveUserProfileFinalized")]
+    partial class RemoveUserProfileFinalized
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/Bennetr.BrickInv.Api/Bennetr.BrickInv.Api/Migrations/BrickInv/20240320183803_RemoveUserProfileFinalized.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bennetr.BrickInv.Api.Migrations.BrickInv
+{
+    /// <inheritdoc />
+    public partial class RemoveUserProfileFinalized : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Finalized",
+                table: "UserProfiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PartId",
+                table: "Parts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Finalized",
+                table: "UserProfiles",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PartId",
+                table: "Parts",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
