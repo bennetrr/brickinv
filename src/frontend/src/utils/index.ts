750623e6cf29fd58ac950a29c74099c6741994e7
bennetrr <bennet@bennetr.me>
2024-07-22 20:31:42 +0200

    chore(frontend): Refactor

--- src/frontend/src/utils/index.ts
+++ src/frontend/src/utils/index.ts
@@ -1,9 +1,10 @@
 import MSTDateTime from './MSTDateTime';
+import redirect from './RedirectRoute';
 import useAsyncEffect from './UseAsyncEffect';
 
-export { MSTDateTime, useAsyncEffect };
+export { MSTDateTime, redirect, useAsyncEffect };
 
-export * from './routes';
 export * from './MSTDateTime';
 export * from './MstFlow';
-export * from './UseAsyncEffect'
+export * from './RedirectRoute';
+export * from './UseAsyncEffect';
