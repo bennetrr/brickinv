478d57941be4d72d22924ef448bde32b673a0e7a
bennetrr <bennet@bennetr.me>
2024-07-23 17:04:26 +0200

    chore(frontend): Add toasts

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { observer } from 'mobx-react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ApiServiceFactory, useAppStore } from './domain';
-import appRoutes from './App.routes.tsx';
 import { useClerk } from '@clerk/clerk-react';
-import { useAsyncEffect } from './utils';
+import { Toast } from 'primereact/toast';
+import { ApiServiceFactory, useAppStore } from './domain';
+import { ToastProvider, useAsyncEffect } from './utils';
+import appRoutes from './App.routes.tsx';
 
 const appRouter = createBrowserRouter(appRoutes);
 
 const App: React.FC = observer(() => {
   const clerk = useClerk();
   const { setStore } = useAppStore();
+
+  const toast = useRef<Toast>(null);
 
   useAsyncEffect(async () => {
     const token = await clerk.session?.getToken();
@@ -22,7 +25,14 @@ const App: React.FC = observer(() => {
     }
   }, [clerk.session]);
 
-  return <RouterProvider router={appRouter} />;
+  return (
+    <>
+      <ToastProvider value={toast.current}>
+        <RouterProvider router={appRouter} />
+      </ToastProvider>
+      <Toast ref={toast}/>
+    </>
+  );
 });
 
 export default App;
--- src/frontend/src/utils/index.ts
+++ src/frontend/src/utils/index.ts
@@ -7,4 +7,5 @@ export { MSTDateTime, redirect, useAsyncEffect };
 export * from './MSTDateTime';
 export * from './MstFlow';
 export * from './RedirectRoute';
+export * from './ToastContext';
 export * from './UseAsyncEffect';
