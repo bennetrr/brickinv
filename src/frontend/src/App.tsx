43cb41db0d624a6cb7b19db2ffc69a08d7c05349
bennetrr <bennet@bennetr.me>
2024-07-23 21:47:41 +0200

    fix(frontend): API call authentication

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,29 +1,14 @@
 import React, { useRef } from 'react';
 import { observer } from 'mobx-react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { useClerk } from '@clerk/clerk-react';
 import { Toast } from 'primereact/toast';
-import { ApiServiceFactory, useAppStore } from './domain';
-import { ToastProvider, useAsyncEffect } from './utils';
+import { ToastProvider } from './utils';
 import appRoutes from './App.routes.tsx';
 
 const appRouter = createBrowserRouter(appRoutes);
 
 const App: React.FC = observer(() => {
-  const clerk = useClerk();
-  const { setStore } = useAppStore();
-
   const toast = useRef<Toast>(null);
-
-  useAsyncEffect(async () => {
-    const token = await clerk.session?.getToken();
-
-    ApiServiceFactory.axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-    if (token) {
-      await setStore.querySets();
-    }
-  }, [clerk.session]);
 
   return (
     <>
